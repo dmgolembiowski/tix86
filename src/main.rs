6e143ce25618abbf0ab3af771cde9c476b94d63a
dmgolembiowski <dmgolembiowski@gmail.com>
2026-02-04 21:24:36 -0500

    Mathematical expression grammar static compiler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use chumsky::prelude::*;
+use dedent::dedent;
 use reedline::{DefaultPrompt, Reedline, Signal};
+use std::convert::AsRef;
 
 #[derive(Clone, Debug, PartialEq)]
 enum Token {
@@ -112,7 +114,33 @@ fn pemdas<'a>() -> impl Parser<'a, &'a [Token], f64> {
         sum
     })
 }
-fn main() {
+fn main() -> std::io::Result<()> {
+    if std::env::args().len() < 2 {
+        return repl_main();
+    } else {
+        let path = std::env::args().skip(1).next().unwrap();
+        let program = std::fs::read_to_string(path.clone()).unwrap();
+        if let Ok(ast_vec) = lexer().parse(&program[..]).into_result() {
+            match pemdas().parse(&ast_vec).into_result() {
+                Ok(res) => {
+                    std::fs::remove_file(&path).expect("it to still be on disk");
+                    let dest = Some(path);
+                    let val = format!("{}", res);
+                    return gen_executable(dest.as_ref(), &val[..]);
+                }
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    std::process::exit(1)
+                }
+            }
+        } else {
+            eprintln!("Compilation failed. Cannot parse source file as an arithmetic program.");
+            std::process::exit(1)
+        }
+    }
+}
+
+fn repl_main() -> std::io::Result<()> {
     let mut line_editor = Reedline::create();
     let prompt = DefaultPrompt::default();
     loop {
@@ -133,4 +161,65 @@ fn main() {
             _ => {}
         }
     }
+    Ok(())
+}
+
+#[rustfmt::skip]
+fn gen_executable(dest: Option<&impl AsRef<std::path::Path>>, val: &str) -> std::io::Result<()> {
+    use std::path::PathBuf;
+    let dest = {
+        if let Some(path) = dest {
+            path.as_ref().to_path_buf()
+        } else {
+            PathBuf::from("program.asm")
+        }
+    };
+    let asm = format!(
+        dedent!(r#"
+            section    .text
+            global     _start
+            _start:
+                mov edx, len
+                mov ecx, msg
+
+                mov ebx, 1
+
+                mov eax, 4
+                int 0x80
+
+                mov eax, 1
+                int 0x80
+
+            section    .data
+                msg    db "{}"
+                len    equ $ -msg"#
+        ),
+        val
+    );
+    let mut obj = dest.clone();
+    obj.set_extension("o");
+    let mut executable = dest.clone();
+    executable.set_extension("");
+    
+    std::fs::write(dest.clone(), asm)?;
+    
+    std::process::Command::new("nasm")
+        .args(&[
+            "-f", "elf32",
+            "-o", obj.clone().to_str().expect("non-empty"),
+            dest.to_str().expect("non-empty")
+        ])
+        .spawn()?;
+
+    std::process::Command::new("ld")
+        .args(&[
+            "-m", "elf_i386",
+            "-o", executable.to_str().expect("non-empty"),
+            obj.clone().to_str().expect("non-empty") 
+        ])
+        .spawn()?;
+
+    std::fs::remove_file(obj)?;
+    
+    Ok(())
 }
